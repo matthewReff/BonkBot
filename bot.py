401d632ec4634fde186cc0559fa75de89db64e37
Matthew Reff <geniusammyralltheothernamesare@gmail.com>
2022-11-19 21:04:15 -0600

    Add skeleton for first three commands

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from discord import Option, ApplicationContext, SlashCommandOptionType
 import sys
 
 intents = discord.Intents.default()
@@ -7,9 +8,27 @@ intents.members = True
 
 bot = commands.Bot(intents=intents)
 
-@bot.slash_command(name="first_slash", guild_ids=['467887610124042240']) #Add the guild ids in which the slash command will appear. If it should be in all, remove the argument, but note that it will take some time (up to an hour) to register the command if it's for all guilds.
-async def first_slash(ctx): 
-    await ctx.respond("You executed the slash command!")
+@bot.slash_command(name="jail", guild_only = True, description="Send a user to jail for an offense")
+async def jail(
+    ctx: ApplicationContext,
+    user: Option(SlashCommandOptionType.user, "Who should go to jail"),
+    offense: Option(str, "What they should go to jail for")
+):
+    await ctx.respond(f"{user.id} {offense} {ctx.guild_id}")
+
+@bot.slash_command(name="addoffense", guild_only = True, description="Add a new jailable offense")
+async def addOffense(
+    ctx: ApplicationContext,
+    offense: Option(str, "Name of the offense you want to add")
+):
+    await ctx.respond(f"{offense} {ctx.guild_id}")
+
+@bot.slash_command(name="rapsheet", guild_only = True, description="Get a list of offenses for the user")
+async def rapSheet(
+    ctx: ApplicationContext,
+    user: Option(SlashCommandOptionType.user, "Whose rapsheet to find")
+):
+    await ctx.respond(f"{user.id} {ctx.guild_id}")
     
 if len(sys.argv) != 2:
     print("You need to provide the bot token when you startup the bot")
